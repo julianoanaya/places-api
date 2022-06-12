89cbb8d3b70a3f90367cbe38ce4679f0be092df6
Julian Anaya <juliananaya531@gmail.com>
2022-06-12 08:16:58 -0700

    made simple place backend with actions

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Place.create!(name: "Disneyland Park", address: "1313 Disneyland Dr, Anaheim, CA 92802")
+Place.create!(name: "Aliso Beach", address: "Laguna Beach, CA 92651")
+Place.create!(name: "Frank & Son Collectible Show", address: "17835 Gale Ave, Rowland Heights, CA 91748")
